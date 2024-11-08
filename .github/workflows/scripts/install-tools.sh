48d4417128ab82cca1113f5529eabccf7c23386d
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-11-08 10:39:19 +0100

    Move scripts into dedicated files

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 
-vm_name="$1"
-vm_image="$2"
-branch_name="$3"
-repo_url="$4"
-script_path="$5"
+scripts_path="$1"
+vm_name="$2"
+vm_image="$3"
+branch_name="$4"
+repo_url="$5"
 
-"$script_path/brew-fetch.sh" "$vm_name" "swiftlint shellcheck markdownlint-cli yamllint ffmpeg"
+if [[ -z $scripts_path || -z $vm_name || -z $vm_image || -z $branch_name || -z $repo_url ]]
+then
+    echo "[!] Usage: $(echo $0) <scripts_path> <vm_name> <vm_image> <branch_name> <repo_url>"
+    exit 1
+fi
 
-"$script_path/create-vm-for-project.sh" "$vm_name" "$vm_image"
-"$script_path/clone-repo-in-vm.sh" "$vm_name" "$branch_name" "$repo_url"
 
-"$script_path/run-vm-shell-command.sh" "$vm_name" "brew install --quiet swiftlint shellcheck markdownlint-cli yamllint ffmpeg"
-"$script_path/run-vm-shell-command.sh" "$vm_name" "rbenv install --skip-existing 3.3.5"
-"$script_path/run-vm-shell-command.sh" "$vm_name" "echo 'export PATH=\"$HOME/.rbenv/bin:\$PATH\"' >> ~/.zshrc"
-"$script_path/run-vm-shell-command.sh" "$vm_name" "rbenv init - >> ~/.zshrc"
-"$script_path/run-vm-shell-command.sh" "$vm_name" "rbenv global 3.3.5"
-"$script_path/run-vm-shell-command.sh" "$vm_name" "sudo gem install bundler -v 2.5.22"
-"$script_path/run-vm-shell-command.sh" "$vm_name" "curl --location https://github.com/shaka-project/shaka-packager/releases/download/v3.2.0/packager-osx-arm64 -o /opt/homebrew/bin/shaka-packager"
-"$script_path/run-vm-shell-command.sh" "$vm_name" "chmod a+x /opt/homebrew/bin/shaka-packager"
-"$script_path/run-vm-shell-command.sh" "$vm_name" "source ~/.zshrc"
+tools="swiftlint shellcheck markdownlint-cli yamllint ffmpeg"
+ruby_version="3.3.5"
+bundler_version="2.5.22"
+
+$scripts_path/brew-fetch.sh $vm_name $tools
+
+$scripts_path/create-vm-for-project.sh $vm_name $vm_image
+$scripts_path/clone-repo-in-vm.sh $vm_name $branch_name $repo_url
+
+$scripts_path/run-vm-shell-command.sh $vm_name "brew install --quiet $tools"
+$scripts_path/run-vm-shell-command.sh $vm_name "rbenv install --skip-existing $ruby_version"
+$scripts_path/run-vm-shell-command.sh $vm_name "echo 'export PATH=\"$HOME/.rbenv/bin:\$PATH\"' >> ~/.zshrc"
+$scripts_path/run-vm-shell-command.sh $vm_name "rbenv init - >> ~/.zshrc"
+$scripts_path/run-vm-shell-command.sh $vm_name "rbenv global $ruby_version"
+$scripts_path/run-vm-shell-command.sh $vm_name "sudo gem install bundler -v $bundler_version"
+$scripts_path/run-vm-shell-command.sh $vm_name "curl --location https://github.com/shaka-project/shaka-packager/releases/download/v3.2.0/packager-osx-arm64 -o /opt/homebrew/bin/shaka-packager"
+$scripts_path/run-vm-shell-command.sh $vm_name "chmod a+x /opt/homebrew/bin/shaka-packager"
+$scripts_path/run-vm-shell-command.sh $vm_name "source ~/.zshrc"
---
+++ .github/workflows/scripts/setup-project.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+scripts_path="$1"
+vm_name="$2"
+github_token="$3"
+
+if [[ -z $scripts_path || -z $vm_name || -z $github_token ]]
+then
+    echo "[!] Usage: $(echo $0) <scripts_path> <vm_name> <github_token>"
+    exit 1
+fi
+
+repository_path="~/_repo"
+keychain_password="admin"
+keychain_path="~/Library/Keychains/login.keychain-db"
+certificate_path="$repository_path/Configuration/6YXTQTG8JJ_development.p12"
+certificate_password="6YXTQTG8JJ"
+
+configuration_repo="github.com/SRGSSR/pillarbox-apple-configuration.git"
+configuration_dir_path="$repository_path/Configuration"
+configuration_script="$repository_path/Scripts/checkout-configuration.sh"
+configuration_branch="certificate"
+configuration_commit="HEAD"
+
+function unlock_keychain {
+    security unlock-keychain -p $keychain_password $keychain_path
+}
+
+function import_certificate {
+    security import $certificate_path -k $keychain_path -P $certificate_password -T /usr/bin/security -T /usr/bin/codesign
+}
+
+function add_certificate_to_keychain {
+    unlock_keychain && import_certificate
+}
+
+function authorize_access_to_certificate_private_key {
+    security set-key-partition-list -S apple-tool:,apple: -s -k $keychain_password $keychain_path
+}
+
+$scripts_path/run-vm-shell-command.sh $vm_name "rm -rf $configuration_dir_path"
+$scripts_path/run-vm-shell-command.sh $vm_name "$configuration_script https://$github_token@$configuration_repo $certificate $configuration_commit $configuration_dir_path"
+$scripts_path/run-vm-shell-command.sh $vm_name add_certificate_to_keychain
+$scripts_path/run-vm-shell-command.sh $vm_name authorize_access_to_certificate_private_key
+$scripts_path/run-vm-shell-command.sh $vm_name "cd $repository_path && make setup"
