ac0cffbcab557e7af22f03f915970f71cd94d6ee
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-11-11 15:40:00 +0100

    Fix keychain management

--- .github/workflows/scripts/unlock-keychain.sh
+++ .github/workflows/scripts/unlock-keychain.sh
@@ -3,6 +3,4 @@
 keychain_password="admin"
 keychain_path="~/Library/Keychains/login.keychain-db"
 
-function unlock_keychain {
-    security unlock-keychain -p $keychain_password $keychain_path
-}
+unlock_keychain="security unlock-keychain -p $keychain_password $keychain_path"
