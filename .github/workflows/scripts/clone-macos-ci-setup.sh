8c2c4ac69822271567c55ec040b93ab67f4c8e32
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-11-12 07:57:45 +0100

    Rename branch to git_branch (#2)
    * Rename branch to git_branch
    
    * Add a specifique path for archives
    
    https://github.com/SRGSSR/fake-and-temporary-pa-for-runners/actions/runs/11785060498/job/32826426268

--- .github/workflows/scripts/clone-macos-ci-setup.sh
+++ .github/workflows/scripts/clone-macos-ci-setup.sh
@@ -2,13 +2,13 @@
 
 scripts_path_destination=$1
 github_token=$2
-branch=$3
+git_branch=$3
 
-if [[ -z $scripts_path_destination || -z $github_token || -z $branch ]]
+if [[ -z $scripts_path_destination || -z $github_token || -z $git_branch ]]
 then
-    echo "[!] Usage: $(echo $0) <scripts_path_destination> <github_token> <branch>"
+    echo "[!] Usage: $(echo $0) <scripts_path_destination> <github_token> <git_branch>"
     exit 1
 fi
 
 rm -rf $scripts_path_destination
-git clone -b $branch "https://$github_token@github.com/SRGSSR/macos-ci-setup.git" $scripts_path_destination
+git clone -b $git_branch "https://$github_token@github.com/SRGSSR/macos-ci-setup.git" $scripts_path_destination
