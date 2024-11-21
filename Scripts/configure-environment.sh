4d5fb52e86abdb7e696e46a94e640d3cb083d51a
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-11-21 18:14:02 +0100

    Add a step for the project configuration

--- Scripts/configure-environment.sh
+++ Scripts/configure-environment.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-apple_api_key_p8="$1"
+set -x
+
+apple_api_key_b64="$1"
 apple_account_info_b64="$2"
 
-if [[ -z $apple_api_key_p8 || -z $apple_account_info_b64 ]]
+if [[ -z $apple_api_key_b64 || -z $apple_account_info_b64 ]]
 then
-    echo "[!] Usage: $0 <apple_api_key_p8> <apple_account_info_b64>"
+    echo "[!] Usage: $0 <apple_api_key_b64> <apple_account_info_b64>"
     exit 1
 fi
 
-mkdir -p ../Configuration
-echo "$apple_account_info_b64" | base64 --decode > ../Configuration/.env
-echo "$apple_api_key_p8" > ../Configuration/AppStoreConnect_API_Key.p8
+mkdir -p Configuration
+echo "$apple_account_info_b64" | base64 --decode > Configuration/.env
+echo "$apple_api_key_b64" | base64 --decode > Configuration/AppStoreConnect_API_Key.p8
