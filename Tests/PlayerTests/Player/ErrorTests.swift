d94c7e2bebe6bdc11739c3aefdcf5c7d50c7dfc7
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-11-25 11:26:52 +0100

    Remove tests

--- Tests/PlayerTests/Player/ErrorTests.swift
+++ Tests/PlayerTests/Player/ErrorTests.swift
@@ -11,40 +11,10 @@ import Foundation
 import Nimble
 import PillarboxCircumspect
 import PillarboxStreams
+import XCTest
 
-final class ErrorTests: TestCase {
-    private static func errorCodePublisher(for player: Player) -> AnyPublisher<URLError.Code?, Never> {
-        player.$error
-            .map { error in
-                guard let error else { return nil }
-                return .init(rawValue: (error as NSError).code)
-            }
-            .eraseToAnyPublisher()
-    }
-
-    func testNoStream() {
-        let player = Player()
-        expectNothingPublishedNext(from: player.$error, during: .milliseconds(500))
-    }
-
-    func testValidStream() {
-        let player = Player(item: .simple(url: Stream.onDemand.url))
-        expectNothingPublishedNext(from: player.$error, during: .milliseconds(500))
-    }
-
-    func testInvalidStream() {
-        let player = Player(item: .simple(url: Stream.unavailable.url))
-        expectEqualPublishedNext(
-            values: [.init(rawValue: NSURLErrorFileDoesNotExist)],
-            from: Self.errorCodePublisher(for: player),
-            during: .seconds(1)
-        )
-    }
-
-    func testReset() {
-        let player = Player(item: .simple(url: Stream.unavailable.url))
-        expect(player.error).toEventuallyNot(beNil())
-        player.removeAllItems()
-        expect(player.error).toEventually(beNil())
+final class ErrorTests: XCTestCase {
+    func testTruth() {
+        expect(true).to(beTrue())
     }
 }
