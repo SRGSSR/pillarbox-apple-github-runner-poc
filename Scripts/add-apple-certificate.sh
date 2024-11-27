c7003a7ec4c6f631f82a12f7ac95d4dc2608d156
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-11-27 15:45:30 +0100

    Use secrets for ENV variables

--- Scripts/add-apple-certificate.sh
+++ Scripts/add-apple-certificate.sh
@@ -2,20 +2,20 @@
 
 root_dir="$1"
 keychain_password="$2"
-apple_certificate_b64="$3"
+apple_certificate_base64="$3"
+apple_certificate_password="$4"
 
-if [[ -z $root_dir || -z $keychain_password || -z $apple_certificate_b64 ]]
+
+if [[ -z $root_dir || -z $keychain_password || -z $apple_certificate_base64 || -z $apple_certificate_password ]]
 then
-    echo "[!] Usage: $0 <root_dir> <keychain_password> <apple_certificate_b64 (base64)>"
+    echo "[!] Usage: $0 <root_dir> <keychain_password> <apple_certificate_base64 (base64)> <apple_certificate_password>"
     exit 1
 fi
 
 keychain_path="$root_dir/app-signing.keychain-db"
-
-apple_certificate_password="6YXTQTG8JJ"
 apple_certificate="$root_dir/certificate.p12"
 
-echo -n "$apple_certificate_b64" | base64 --decode -o "$apple_certificate"
+echo -n "$apple_certificate_base64" | base64 --decode -o "$apple_certificate"
 
 # Create a temporary keychain (https://docs.github.com/en/actions/using-github-hosted-runners/using-github-hosted-runners/about-github-hosted-runners)
 security create-keychain -p "$keychain_password" "$keychain_path"
--- Scripts/configure-environment.sh
+++ Scripts/configure-environment.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 apple_api_key_b64="$1"
-apple_account_info_b64="$2"
 
-if [[ -z $apple_api_key_b64 || -z $apple_account_info_b64 ]]
+if [[ -z $apple_api_key_b64 ]]
 then
-    echo "[!] Usage: $0 <apple_api_key_b64> <apple_account_info_b64>"
+    echo "[!] Usage: $0 <apple_api_key_b64>"
     exit 1
 fi
 
 mkdir -p Configuration
-echo "$apple_account_info_b64" | base64 --decode > Configuration/.env
 echo "$apple_api_key_b64" | base64 --decode > Configuration/AppStoreConnect_API_Key.p8
