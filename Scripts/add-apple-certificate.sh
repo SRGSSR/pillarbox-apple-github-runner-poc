7bb5b3801d3a1df414ff60707e1996bd9f8fcc53
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-11-28 14:56:16 +0100

    Improve scripts syntax

--- Scripts/add-apple-certificate.sh
+++ Scripts/add-apple-certificate.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-root_dir="$1"
-keychain_password="$2"
-apple_certificate_base64="$3"
-apple_certificate_password="$4"
+ROOT_DIR="$1"
+KEYCHAIN_PASSWORD="$2"
+APPLE_CERTIFICATE_BASE64="$3"
+APPLE_CERTIFICATE_PASSWORD="$4"
 
-
-if [[ -z $root_dir || -z $keychain_password || -z $apple_certificate_base64 || -z $apple_certificate_password ]]
+if [[ -z $ROOT_DIR || -z $KEYCHAIN_PASSWORD || -z $APPLE_CERTIFICATE_BASE64 || -z $APPLE_CERTIFICATE_PASSWORD ]]
 then
-    echo "[!] Usage: $0 <root_dir> <keychain_password> <apple_certificate_base64 (base64)> <apple_certificate_password>"
+    echo "[!] Usage: $0 <ROOT_DIR> <KEYCHAIN_PASSWORD> <APPLE_CERTIFICATE_BASE64 (base64)> <APPLE_CERTIFICATE_PASSWORD>"
     exit 1
 fi
 
-keychain_path="$root_dir/app-signing.keychain-db"
-apple_certificate="$root_dir/certificate.p12"
+KEYCHAIN_PATH="$ROOT_DIR/app-signing.keychain-db"
+APPLE_CERTIFICATE="$ROOT_DIR/certificate.p12"
 
-echo -n "$apple_certificate_base64" | base64 --decode -o "$apple_certificate"
+echo -n "$APPLE_CERTIFICATE_BASE64" | base64 --decode -o "$APPLE_CERTIFICATE"
 
 # Create a temporary keychain (https://docs.github.com/en/actions/using-github-hosted-runners/using-github-hosted-runners/about-github-hosted-runners)
-security create-keychain -p "$keychain_password" "$keychain_path"
-security set-keychain-settings -lut 21600 "$keychain_path"
-security unlock-keychain -p "$keychain_password" "$keychain_path"
+security create-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+security set-keychain-settings -lut 21600 "$KEYCHAIN_PATH"
+security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
 
 # Import certificate
-security import "$apple_certificate" -k "$keychain_path" -P "$apple_certificate_password" -A -t cert -f pkcs12
+security import "$APPLE_CERTIFICATE" -k "$KEYCHAIN_PATH" -P "$APPLE_CERTIFICATE_PASSWORD" -A -t cert -f pkcs12
 # Authorize access to certificate private key
-security set-key-partition-list -S apple-tool:,apple: -k "$keychain_password" "$keychain_path"
+security set-key-partition-list -S apple-tool:,apple: -k "$KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
 # Set the default keychain
-security list-keychain -d user -s "$keychain_path"
+security list-keychain -d user -s "$KEYCHAIN_PATH"
--- Scripts/configure-environment.sh
+++ Scripts/configure-environment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apple_api_key_b64="$1"
+APPLE_API_KEY_BASE64="$1"
 
-if [[ -z $apple_api_key_b64 ]]
+if [[ -z $APPLE_API_KEY_BASE64 ]]
 then
-    echo "[!] Usage: $0 <apple_api_key_b64>"
+    echo "[!] Usage: $0 <APPLE_API_KEY_BASE64>"
     exit 1
 fi
 
 mkdir -p Configuration
-echo "$apple_api_key_b64" | base64 --decode > Configuration/AppStoreConnect_API_Key.p8
+echo "$APPLE_API_KEY_BASE64" | base64 --decode > Configuration/AppStoreConnect_API_Key.p8
