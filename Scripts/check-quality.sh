eec5d082fac62cdd098897e68aaff0cbf55c4d87
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-11-29 17:10:58 +0100

    Use Ubuntu runner for check-quality and build-documentation jobs

--- Scripts/check-quality.sh
+++ Scripts/check-quality.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-set -e
+set -xe
 
-eval "$(pkgx --shellcode)"
+eval "$(curl -Ssf https://pkgx.sh)"
+
 env +swiftlint +rubocop +shellcheck +markdownlint +yamllint
 
 echo "... checking Swift code..."
 if [ $# -eq 0 ]; then
   swiftlint --quiet --strict
-elif [[ "$1" == "only-changes" ]]; then
+elif [[ "$1" == "--only-changes" ]]; then
   git diff --staged --name-only | grep ".swift$" | xargs swiftlint lint --quiet --strict
 fi
 echo "... checking Ruby scripts..."
