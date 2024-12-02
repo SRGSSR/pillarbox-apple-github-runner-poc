a6e07fbaa137cc6ccfe2be5de923114eba859511
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-12-02 07:18:29 +0100

    Revert "Use Ubuntu runner for check-quality and build-documentation jobs"
    This reverts commit 63453a7a366381da3c03bac27b927da4194f3b3b.

--- Scripts/check-quality.sh
+++ Scripts/check-quality.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-set -xe
+set -e
 
-eval "$(curl -Ssf https://pkgx.sh)"
-
+eval "$(pkgx --shellcode)"
 env +swiftlint +rubocop +shellcheck +markdownlint +yamllint
 
 echo "... checking Swift code..."
 if [ $# -eq 0 ]; then
   swiftlint --quiet --strict
-elif [[ "$1" == "--only-changes" ]]; then
+elif [[ "$1" == "only-changes" ]]; then
   git diff --staged --name-only | grep ".swift$" | xargs swiftlint lint --quiet --strict
 fi
 echo "... checking Ruby scripts..."
